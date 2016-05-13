401e2a692ca4447c03d7763a2b44e4f92cc2f02e
Matthias Henning <Matze_GitHub@gmx.de>
2016-05-13 15:16:02 +0200

    Weiter mit Vorschau

--- PrintStuff.cs
+++ PrintStuff.cs
@@ -6,14 +6,24 @@ using System.Threading.Tasks;
 using Windows.Graphics.Printing;
 using Windows.UI.WebUI;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Printing;
 
 namespace PrintDemo
 {
     public class PrintStuff
     {
+        private PrintDocument _printDocument;
+        private IPrintDocumentSource _printDocumentSource;
+
         public async void Print()
         {
             var manager = PrintManager.GetForCurrentView();
+
+            _printDocument = new PrintDocument();
+            _printDocumentSource = _printDocument.DocumentSource;
+            _printDocument.Paginate += CreatePrintPreviewPages;
+            _printDocument.GetPreviewPage += GetPrintPreviewPage;
+            _printDocument.AddPages += AddPrintPages;
 
             manager.PrintTaskRequested += Manager_PrintTaskRequested;
             try
@@ -35,6 +45,29 @@ namespace PrintDemo
             }
         }
 
+        private void AddPrintPages(object sender, AddPagesEventArgs e)
+        {
+
+        }
+
+        private void GetPrintPreviewPage(object sender, GetPreviewPageEventArgs e)
+        {
+            var doc = GetPrintDoc(sender);
+
+            doc.SetPreviewPage(e.PageNumber, new TextBlock { Text = e.PageNumber.ToString() });
+        }
+
+        private void CreatePrintPreviewPages(object sender, PaginateEventArgs e)
+        {
+            var doc = GetPrintDoc(sender);
+            doc.SetPreviewPageCount(10, PreviewPageCountType.Final);
+        }
+
+        private static PrintDocument GetPrintDoc(object sender)
+        {
+            return (PrintDocument)sender;
+        }
+
         private static async Task ShowInfoDialog(bool result)
         {
             // Printing cannot proceed at this time
@@ -51,7 +84,7 @@ namespace PrintDemo
         {
             var printTask = e.Request.CreatePrintTask("C# Printing SDK Sample", sourceRequested =>
             {
-
+                sourceRequested.SetSource(_printDocumentSource);
 
             });
         }
