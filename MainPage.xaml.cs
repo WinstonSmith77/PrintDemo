3689ae9ab2a7921dc0b8730012f8491e3fb5df0f
Matthias Henning <Matze_GitHub@gmx.de>
2016-05-13 14:40:54 +0200

    Erster Schritt

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Graphics.Printing;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +26,11 @@ namespace PrintDemo
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private  void Button_Click(object sender, RoutedEventArgs e)
+        {
+            new PrintStuff().Print();
         }
     }
 }
---
+++ PrintStuff.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Graphics.Printing;
+using Windows.UI.WebUI;
+using Windows.UI.Xaml.Controls;
+
+namespace PrintDemo
+{
+    public class PrintStuff
+    {
+        public async void Print()
+        {
+            var manager = PrintManager.GetForCurrentView();
+
+            manager.PrintTaskRequested += Manager_PrintTaskRequested;
+            try
+            {
+                try
+                {
+                    var result = await PrintManager.ShowPrintUIAsync();
+
+                    await ShowInfoDialog(result);
+                }
+                catch
+                {
+                    await ShowInfoDialog(false);
+                }
+            }
+            finally
+            {
+                manager.PrintTaskRequested -= Manager_PrintTaskRequested;
+            }
+        }
+
+        private static async Task ShowInfoDialog(bool result)
+        {
+            // Printing cannot proceed at this time
+            ContentDialog noPrintingDialog = new ContentDialog
+            {
+                Title = "Printing",
+                Content = result ? "Print Successful!" : "\nSorry, printing can' t proceed at this time.",
+                PrimaryButtonText = "OK"
+            };
+            await noPrintingDialog.ShowAsync();
+        }
+
+        private void Manager_PrintTaskRequested(PrintManager sender, PrintTaskRequestedEventArgs e)
+        {
+            var printTask = e.Request.CreatePrintTask("C# Printing SDK Sample", sourceRequested =>
+            {
+
+
+            });
+        }
+    }
+}
